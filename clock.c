fece1458aa7dd2f37e1c3199c56cc6fa87252721
Alex <you@example.com>
2020-10-08 13:21:48 +0200

    commit 1

---
+++ clock.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <time.h>
+#include <semaphore.h>
+
+int main(int argc, char const *argv[])
+{
+	while(1){
+
+		
+
+	}
+	return 0;
+}
---
+++ praktika1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <time.h>
+#include <semaphore.h>
+
+
+int main(int argc, char const *argv[])
+{
+	
+	
+}
