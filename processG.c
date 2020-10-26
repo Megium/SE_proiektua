1302e82dba7e8b0a82a9676bf3de50818b8b36ed
Alex de Miguel <alex@MacBook-Air-de-Alex.local>
2020-10-26 13:42:25 +0100

    Commit 9

--- processG.c
+++ processG.c
@@ -10,35 +10,48 @@
 #include "processG.h"
 
 
-
+//Prozesuaren informazioa gordetzeko
 struct pcb
 {
 int pid;
 //aurreago gauza gehio	
 };
 
+//Prozesuak sartzeko ilara, bertan buffer bat prozesuen informazioekin.
 struct queue
 {
-	struct pcb buff[MAX];
+	struct pcb buff[];
 	int indizea;
 };
 
 void generate(int id, struct pcb *prozesu);
 
 void *generateProcess_f(){
+    //prozesuak sortzeko ausazko denboren hazia
 	time_t t;
-	struct queue ilara;
+    srand((unsigned) time(&t));
+    
+    //Ilara objetua sortu eta hasieratu.
+    struct queue ilara;
 	ilara.indizea = 0;
-	int i = 0;
+    //Ilararen tamaina aldakora izango denez, ilara dinamiko bat sortu behar da.
+    ilara.buff = malloc(MAX*sizeof(struct pcb));
+	
+    int i = 0;
 	int d;
-	srand((unsigned) time(&t));
+	
 	while(1){
 
 		d = rand() % MAIZT;
+        //Sorturiko denbora 0 bada salto, erroreak ekiditeko
 		if(d!=0){
+            //Prozesu berri bat sortu, pcb tamainakoa
 			struct pcb prozesu = malloc(1*sizeof(struct pcb));
+            //bufferrean sartu beharreko prozesua sortzeko deia egin
 			ilara.buff[i] = generate(i, prozesu);
+            //eguneratu indizea
 			ilara.indizea++;
+            
 			printf("%d. prozesua sortu da.\n", i);
 			i++;
 		}
@@ -48,6 +61,7 @@ void *generateProcess_f(){
 
 }
 
+//Prozesu berri bat sortu, pcb motako struct berri bat eta bertan honen id-a
 void generate(int id, struct pcb *prozesu){
 	prozesu->pid = id;
 }
